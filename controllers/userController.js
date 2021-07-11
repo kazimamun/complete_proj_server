3919f1606f9c358f3d540eb1eb3d8f0cf71e5b91
kazimamun <kazimamun716@gmail.com>
2021-07-11 20:29:27 +0600

    registration and login working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
 const sql = require("../connection/db");
 const registerValidator = require('../validator/registerValidator');
+const loginValidator = require('../validator/loginValidator');
 const {serverError, resourceError} = require('../utils/error');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 module.exports={
     register(req, res){
         let {name, email, password, confirmPassword, gender, country} = req.body;
@@ -32,7 +34,36 @@ module.exports={
         }        
     },
     login(req, res){
-        
+        let {email, password} = req.body;
+        let validate = loginValidator({email, password});
+        if (!validate.isValid){
+            return res.status(400).json(validate.error)
+        }
+        sql.query(`SELECT * FROM users WHERE email = ?`, [email], (err, user) => {  
+            if (!user.length){
+                return resourceError(res, "user not found")
+            } else { //if user exist on database
+                bcrypt.compare(password, user[0].password, (err, result)=>{
+                    if (err) {
+                        serverError(res, err)
+                    }
+                    if (!result) { // if password not matched
+                        resourceError(res, "Password dosen't matched")
+                    }
+                    //make token for provide user time for login
+                    let token = jwt.sign({
+                        // _id : user._id,
+                        name : user.name,
+                        email : user.email
+                    }, "SECRET", {expiresIn: "2h"})
+
+                    res.status(200).json({
+                        message: "login successfully",
+                        token : `Barer ${token}`
+                    })
+                })
+            }
+        })
     },
     allUsers(req, res){
         sql.query("SELECT * FROM users", (err, result) => {
